1e50dbd1d1be2f4ce33cd3a22b5ee6189e597ae5
Nicklas Schultz <nicklas.schultz@accedo.tv>
2016-09-09 10:17:31 +0200

    Added a start button in rules activity

--- PoolGames/app/src/main/java/com/developergmail/schultz/nicklas/poolgames/Fragments/RulesFragment.java
+++ PoolGames/app/src/main/java/com/developergmail/schultz/nicklas/poolgames/Fragments/RulesFragment.java
@@ -1,14 +1,19 @@
 package com.developergmail.schultz.nicklas.poolgames.Fragments;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.SpannableString;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.developergmail.schultz.nicklas.poolgames.LauncherActivity;
 import com.developergmail.schultz.nicklas.poolgames.R;
+import com.developergmail.schultz.nicklas.poolgames.ResultActivity;
 import com.developergmail.schultz.nicklas.poolgames.games.IGame;
 import com.developergmail.schultz.nicklas.poolgames.helpers.StringHelper;
 
@@ -33,11 +38,20 @@ public class RulesFragment extends Fragment {
         if(section == 2) {
             rootView = inflater.inflate(R.layout.fragment_image_object, container, false);
             TextView txt = (TextView) rootView.findViewById(R.id.rack_text);
-            //txt.append(StringHelper.getLargeText("Game Play"));
             txt.setText(game.getRules().getGamePlay());
         } else {
             rootView = inflater.inflate(R.layout.fragment_image_object, container, false);
             TextView txt = (TextView) rootView.findViewById(R.id.rack_text);
+            ImageButton imgBtn = (ImageButton) rootView.findViewById(R.id.startGame);
+            imgBtn.setVisibility(View.VISIBLE);
+            imgBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getActivity(), ResultActivity.class);
+                    intent.putExtra(LauncherActivity.GAME, game.getName());
+                    getActivity().startActivity(intent);
+                }
+            });
             txt.setText(game.getRules().getWinning());
         }
 
